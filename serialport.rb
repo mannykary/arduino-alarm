7bb92d797c8402566eb9ce91d40958d43a536e76
Manny Karyampudi <mrkaryam@gmail.com>
2016-09-05 20:33:51 +0000

    More refactoring, added debug flag

--- serialport.rb
+++ serialport.rb
@@ -3,7 +3,7 @@ require 'serialport'
 require 'pubnub'
 
 class ArduinoAlarm
-  attr_accessor :sp, :pubnub
+  attr_accessor :sp, :pubnub, :debug
 
   #params for serial port
   PORT_STR = '/dev/ttyACM0'
@@ -16,20 +16,18 @@ class ArduinoAlarm
   SUBSCRIBE_KEY = ENV['PUBNUB_SUB_KEY']
   PUBLISH_KEY = ENV['PUBNUB_PUB_KEY']
   
-  def initialize
+  def initialize(debug: false)
     puts 'Initializing...'
     @sp = SerialPort.new(PORT_STR, BAUD_RATE, DATA_BITS, STOP_BITS, PARITY)
     @pubnub = Pubnub.new(subscribe_key: SUBSCRIBE_KEY, publish_key: PUBLISH_KEY)
+    @debug = debug
   end
 
   def listen
     puts 'Listening...'
 
     while true do
-      message = sp.gets
-      message.chomp!
-      puts message
-      
+      message = get_message          
       case message
         when 'Loud sound detected!'
           publish('loud_noise_notifications', message)
@@ -38,13 +36,20 @@ class ArduinoAlarm
   end 
   
   private
+  def get_message
+    message = sp.gets
+    message.chomp!
+    puts message if debug
+    message
+  end
 
   def publish(channel, message)
+    puts "Publishing message: \"#{message}\" (channel: #{channel})"
     pubnub.publish(
       channel: channel,
       message: { text: message }
     ) do |envelope|
-      puts envelope.status
+      puts envelope.status if debug
     end
   end
 end
