fd0e6b804de9c06d76969bb838da0fc95a75ff9e
Manny Karyampudi <mrkaryam@gmail.com>
2016-09-05 06:49:15 +0000

    Adding ruby script for serial communication

---
+++ serialport.rb
@@ -0,0 +1,19 @@
+require 'rubygems'
+require 'serialport'
+ 
+#params for serial port
+port_str = '/dev/ttyACM0'
+baud_rate = 9600
+data_bits = 8
+stop_bits = 1
+parity = SerialPort::NONE
+ 
+sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
+
+while true do
+  message = sp.gets
+  if message
+    message.chomp!
+    puts message
+  end
+end  
