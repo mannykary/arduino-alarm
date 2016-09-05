25c8e115f2030984a6549c2d7b460979d8640901
Manny Karyampudi <mrkaryam@gmail.com>
2016-09-05 19:51:24 +0000

    Added PubNub publishing

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source "https://rubygems.org"
 
 gem 'serialport', '~> 1.3', '>= 1.3.1'
+gem 'pubnub', '~> 4.0.7'
--- serialport.rb
+++ serialport.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'serialport'
+require 'pubnub'
  
 #params for serial port
 port_str = '/dev/ttyACM0'
@@ -7,13 +8,26 @@ baud_rate = 9600
 data_bits = 8
 stop_bits = 1
 parity = SerialPort::NONE
- 
+
+#Pubnub keys
+subscribe_key = ENV['PUBNUB_SUB_KEY']
+publish_key = ENV['PUBNUB_PUB_KEY']
+
 sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
+pubnub = Pubnub.new(subscribe_key: subscribe_key, publish_key: publish_key)
 
 while true do
   message = sp.gets
   if message
     message.chomp!
     puts message
+
+    pubnub.publish(
+      channel: 'loud_noise_notifications',
+      message: { text: message }
+    ) do |envelope|
+      puts envelope.status
+    end
   end
-end  
+end
+
